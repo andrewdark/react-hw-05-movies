8b010a04d8d9642ad88747beaec1518c4b1ee824
andrewdark <samsonov.a@ukr.net>
2023-10-20 14:22:15 +0300

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import {Home} from "./Home/Home";
+
+const APP_PATH = '/goit-react-hw-05-movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
+    <div >
+      <nav style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
+      }}>
+        <Link to={`${APP_PATH}/`} style={{ marginRight: 24 }}>
+          Home
+        </Link>
+        <Link to={`${APP_PATH}/movies`} style={{ marginRight: 24 }}>
+          Movies
+        </Link>
+        <Link to={`${APP_PATH}/movies/1`} style={{ marginRight: 24 }}>
+          MovieDetails
+        </Link>
+      </nav>
+      <Routes>
+        <Route path={`${APP_PATH}/`} element={<Home />} />
+        <Route path={`${APP_PATH}/movies`} element={<Home />} />
+        <Route path={`${APP_PATH}/movies/:movieId`} element={<Home />}>
+          {/*<Route path="cast" element={<Cast />} />*/}
+          {/*<Route path="reviews" element={<Reviews />} />*/}
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export const Home = props => {
+  return (
+    <div>
+      <h1>HELLO WORM!</h1>
+    </div>
+  );
+};
