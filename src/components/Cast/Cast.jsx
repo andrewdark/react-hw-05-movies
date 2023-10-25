4a47475731fa36d37483d348f7d6583566522d9f
andrewdark <samsonov.a@ukr.net>
2023-10-25 16:01:51 +0300

    add casts

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getCast } from '../../services/api';
+import noimg from '../../img/No_img.jpg';
+
 const Cast = () => {
+  const { movieId } = useParams();
+  const [theCast, setTheCast] = useState([]);
+
+  useEffect(() => {
+    getCast(movieId)
+      .then(data => {
+        setTheCast(data.cast);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [movieId]);
 
   return (
     <div>
+      <ul>
+        {theCast.map(el => {
+          return (
+            <li key={el.id}>
+              <img
+                src={
+                  el.profile_path !== null
+                    ? 'https://image.tmdb.org/t/p/w200/' + el.profile_path
+                    : noimg
+                }
+                alt=""
+              />
+              {el.name} AS {el.character}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
