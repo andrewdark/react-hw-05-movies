cd28b99e6eebe9a2c382998328c087f09964b847
andrewdark <samsonov.a@ukr.net>
2023-10-25 16:01:12 +0300

    add reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { getReviews } from '../../services/api';
+import { useState, useEffect } from 'react';
+
 const Reviews = () => {
+  const { movieId } = useParams();
+  const [theReview, setTheReview] = useState([]);
+
+  useEffect(() => {
+    getReviews(movieId)
+      .then(data => {
+        setTheReview(data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [movieId]);
 
   return (
     <div>
+      {theReview.length > 0 && (
+        <ul>
+          {theReview.map(el => {
+            return (
+              <li key={el.id}>
+                {el.author}
+                <br />
+                <p>{el.content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
